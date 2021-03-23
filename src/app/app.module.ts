de8e229d7ebc60bcc5ffde806dfc7f4a91840383
pheetah <eyupfatih.ersoy@hotmail.com>
2021-03-23 23:49:16 +0300

    project template update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoutingModule } from './routing-module/routing-module.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from './material-module';
+import { MaterialModule } from './material/material-module';
+import { HomePageModule } from './home-page/home-page.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
- 
   ],
   imports: [
     BrowserModule,
     RoutingModule,
+    HomePageModule,
     BrowserAnimationsModule,
     MaterialModule,
     ReactiveFormsModule,
   ],
+  exports:[MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -2,27 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home-component/home-component.component';
 import { SignComponent } from './sign/sign.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
-import { MatSliderModule } from '@angular/material/slider';
-import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material-module';
+import { RoutingModule } from '../routing-module/routing-module.module';
 
 
 @NgModule({
   declarations: [HomeComponent, SignComponent],
   imports: [
     CommonModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    MatSliderModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RoutingModule,
+    BrowserModule,
+    CommonModule,
     ReactiveFormsModule
-  ]
+  ],
+  bootstrap: [HomeComponent]
 })
 export class HomePageModule { }
