d2fe2bf5a7b5cd2422b49a63c90ebf4b025921fb
pheetah <eyupfatih.ersoy@hotmail.com>
2021-05-25 20:25:16 +0300

    added file checks

--- src/app/home-page/documents/documents.component.ts
+++ src/app/home-page/documents/documents.component.ts
@@ -21,6 +21,28 @@ export class DocumentsComponent{
   uploadFile(event:any) {
     this.file = (event.target).files[0];
     console.log(this.file);
+    var re = /pdf/gi;
+    var re2 = /zip/gi;
+    var re3 = /docx/gi;
+    var re4 = /rar/gi;
+    
+    if (this.file.size<20000000) 
+    {
+        document.getElementsByClassName('Fsize')[0].innerHTML= "";
+    }
+
+    if (this.file.size>=20000000) {
+      document.getElementsByClassName('Fsize')[0].innerHTML="This file is bigger than 20 MB. Please upload file under 20 MB";
+      console.log('This file is bigger than 20 MB. Please upload file under 20 MB');
+    }
+  
+    if(this.file.name.search(re)== -1 &&this.file.name.search(re2)== -1&& this.file.name.search(re3)== -1&&this.file.name.search(re4)== -1){
+    document.getElementsByClassName('Ftype')[0].innerHTML="Invalid Type";
+    }
+    else {
+      document.getElementsByClassName('Ftype')[0].innerHTML= "";
+    }
+
   }
 
   upload(){
