ab56cd2786f0c67f74e4cb4013b11095bedbbe14
pheetah <eyupfatih.ersoy@hotmail.com>
2021-06-01 12:41:09 +0300

    added dashboard page

--- src/app/home-page/dashboard/dashboard.component.ts
+++ src/app/home-page/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  ngOnInit(){}
+  displayedColumns = ['item', 'cost'];
+  transactions: Transaction[] = [
+    {item: 'Beach ball', cost: 4},
+    {item: 'Towel', cost: 5},
+    {item: 'Frisbee', cost: 2},
+    {item: 'Sunscreen', cost: 4},
+    {item: 'Cooler', cost: 25},
+    {item: 'Swim suit', cost: 15},
+  ];
 
-  ngOnInit(): void {
+  /** Gets the total cost of all transactions. */
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
   }
+}
 
+export interface Transaction {
+  item: string;
+  cost: number;
 }
